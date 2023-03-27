e1e39ced201316359536c4bb114f948616e35b6e
JerrinThomas <jerrinthomasonline@gmail.com>
2023-03-27 09:53:46 -0400

    Checkin changes for notification enable.

--- app/src/main/java/com/example/tutornite/activities/SettingsActivity.java
+++ app/src/main/java/com/example/tutornite/activities/SettingsActivity.java
@@ -1,24 +1,26 @@
 package com.example.tutornite.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.Gravity;
 import android.widget.CheckBox;
-import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.example.tutornite.R;
+import com.example.tutornite.utils.Constants;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class SettingsActivity extends BaseActivity {
 
     ImageView back_img;
     CheckBox notification_chkbox;
-
-
+    LinearLayout enable_notification_nav, about_layout, support_layout;
 
     private FirebaseAuth mAuth;
+    FirebaseFirestore db;
+    FirebaseUser currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +28,8 @@ public class SettingsActivity extends BaseActivity {
         setContentView(R.layout.activity_settings);
 
         mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+        currentUser = mAuth.getCurrentUser();
 
         initViews();
     }
@@ -34,10 +38,29 @@ public class SettingsActivity extends BaseActivity {
         back_img = findViewById(R.id.back_img);
         notification_chkbox = findViewById(R.id.notification_chkbox);
 
+        enable_notification_nav = findViewById(R.id.enable_notification_nav);
+        about_layout = findViewById(R.id.about_layout);
+        support_layout = findViewById(R.id.support_layout);
+
+        notification_chkbox.setOnCheckedChangeListener((compoundButton, isChecked) ->
+        {
+            if(isChecked) {
+
+            }
+        });
+
         back_img.setOnClickListener(view -> {
             onBackPressed();
         });
 
+        about_layout.setOnClickListener(view -> {
+            openWebPage(Constants.rating_app_url);
+        });
+
+        support_layout.setOnClickListener(view -> {
+            openWebPage(Constants.rating_app_url);
+        });
 
     }
+
 }
